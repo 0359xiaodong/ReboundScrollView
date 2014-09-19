78a515df7a0500214ec8e47e6b066e3d84c93d50
tangmingcong <tangmingcong@tangmingcong.sen5.sz>
2014-09-19 16:35:12 +0800

    push code

---
+++ Demo/src/com/thomas/demo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.thomas.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity
+{
+	private static final String TAG = MainActivity.class.getSimpleName();
+	View v = null;
+	TextView seg = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
